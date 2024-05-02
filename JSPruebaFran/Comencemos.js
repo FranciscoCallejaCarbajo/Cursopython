c597df989816d05b9d5112749e70fdf1530ba2ac
FranciscoCallejaCarbajo <160488986+FranciscoCallejaCarbajo@users.noreply.github.com>
2024-05-02 09:48:29 +0200

    update js

--- JSPruebaFran/Comencemos.js
+++ JSPruebaFran/Comencemos.js
@@ -1,2 +1,38 @@
+/*Variable normal global var*/
 var nombre = "Fran";
-alert("tu nombre es:" + nombre)
+alert("tu nombre es: " + nombre);
+
+/*Variable de corto alcance Let*/
+
+let numero = 29;
+
+alert(numero);
+
+/*Variable const: esta variable solo se define una vez y no mas, es decir una vez que le das su primer valor por mucho que la intentes cambiar en otro momento no vas a poder*/
+/*Esta variable se tiene que inicializar en la misma linea ya que se tiene que dejar claro el valor de la constate en ese mismo momento*/
+const num = 2;
+
+alert (numero);
+
+/*En el curso que estoy haciendo trabajaremos con let */
+/*Otra manera de nombrar variables es asi */
+
+let numero1 = 1, numero2 = 40, numero3 = 25;
+
+alert (numero1);
+alert (numero2);
+alert (numero3);
+
+
+/*Hoisting
+habla acerca de como funciona una cosa y como funciona la otra 
+*/
+/*Null*/
+let nombre = null;
+
+/*NaN*/
+let num1 = 4;
+
+let num2 = 2;
+
+alert (num1 + num2);
