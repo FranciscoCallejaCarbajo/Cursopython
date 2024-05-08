b4636aab419bdee384cdf04b07fe211d1f76f766
fran <franciscocallejacarbajo17@gmail.com>
2024-05-08 18:00:12 +0200

    update clase

---
+++ Js/Cliente/ejercicio.js
@@ -0,0 +1,16 @@
+
+//input : 100,21
+//output: 121
+
+
+let dinero = 200;
+
+const iva = 21;
+
+let ibaProducto = dinero*iva/100;
+
+ let resultado = ibaProducto + dinero;
+
+ 
+ console.log(resultado);
+
