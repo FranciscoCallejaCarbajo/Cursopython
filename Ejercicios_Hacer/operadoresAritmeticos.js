5711e1e6cb0be417f81e334ce76f961503e78c78
fran <franciscocallejacarbajo17@gmail.com>
2024-05-16 09:36:17 +0200

    update ejercicios clase

--- Ejercicios_Hacer/operadoresAritmeticos.js
+++ Ejercicios_Hacer/operadoresAritmeticos.js
@@ -164,3 +164,81 @@ let resultado1_4 = contador * 2
 
 
 document.getElementById("eje-1.5").innerText = `El resultado es: ${resultado1_4}`;
+
+//Ejercicio 4
+
+let variable_y = 15;
+
+let resultado1_1_1 = variable_y / 3;
+document.getElementById("eje-1.5.1").innerText = `El resultado es: ${resultado1_1_1}`;
+let resultado1_1_2 = variable_y / 5;
+document.getElementById("eje-1.5.2").innerText = `El resultado es: ${resultado1_1_2}`;
+let resultado1_1_3 = variable_y / 7;
+document.getElementById("eje-1.5.3").innerText = `El resultado es: ${resultado1_1_3}`;
+let resultado1_1_4 = variable_y / 10;
+document.getElementById("eje-1.5.4").innerText = `El resultado es: ${resultado1_1_4}`;
+
+let resultado_resto = resultado1_1_1 % 1;
+document.getElementById("eje-1.5.1.1").innerText = `El resto es: ${resultado_resto}`;
+let resultado_resto1 = resultado1_1_4 % 1;
+document.getElementById("eje-1.5.1.2").innerText = `El resto es: ${resultado_resto1}`;
+
+//Ejercicio 5
+
+let saldo = 100;
+
+let resultado1_2_1 = saldo * 0.90;
+document.getElementById("eje-1.6.1").innerText = `El resultado es: ${resultado1_2_1}`;
+let resultado1_2_2 = saldo - 15;
+document.getElementById("eje-1.6.2").innerText = `El resultado es: ${resultado1_2_2}`;
+let resultado1_2_3 = saldo * 1.125;
+document.getElementById("eje-1.6.3").innerText = `El resultado es: ${resultado1_2_3}`;
+let resultado1_2_4 = saldo / 2;
+document.getElementById("eje-1.6.4").innerText = `El resultado es: ${resultado1_2_4}`;
+
+//OPERADORES DE COMPARACION
+
+//Ejercicio 1
+console.log("OPERADORES DE COMPARACIÓN")
+console.log(7 > 3); //7 es mayor que 3 eso es true 
+console.log(3 < 7); //3 es menor que 7 eso es true
+console.log(1 == '1'); // 1 es == que 1 no tiene encuenta el tipo de dato asique es true
+console.log(1 === '1'); // 1 es === que '1' aqui sale false ya que con el triple = estamos haciendo una comparacion estricta 
+console.log(3 != '3'); //aqui devuelve false por que 3 es = a 3 y aqui estas haciendo una conversion de tipo implicito
+console.log(3 !== 3.0);// aqui devuelve false tambien pero estamos haciendo una comparacion estricta  y javascrip tiene encuenta tanto el valor como el tipo de dato y aunque los dos tengan el mismo valor unoo es un numero entero y otro tiene decimales.
+
+//Ejercicio 2
+
+let edad = Number(prompt("Introduce tu edad"))
+
+let comparacion1 = edad > 18 ;
+
+document.getElementById("eje-1.7.1").innerText = `¿Es mayor de edad?: ${comparacion1}`;
+
+let comparacion2 = edad < 18 ;
+
+document.getElementById("eje-1.7.2").innerText = `¿Es menor de edad?: ${comparacion2}`;
+
+let comparacion3 = edad >= 25 && edad <= 35 ;
+
+document.getElementById("eje-1.7.3").innerText = `¿Está entre 25 y 35 años?: ${comparacion3}`;
+
+let comparacion4 = edad > 65 ;
+
+document.getElementById("eje-1.7.4").innerText = `¿No es mayor de 65 años?: ${comparacion4}`;
+
+let comparacion5 = edad >= 13 && edad <= 24 ;
+
+document.getElementById("eje-1.7.5").innerText = `Tiene entre 13 y 24 años cumplidos: ${comparacion5}`;
+
+
+
+
+
+
+
+
+
+
+
+
